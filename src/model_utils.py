619072994d811be85ad06f83e13c1d5255748b8a
Eric Cody <ericcody1@gmail.com>
2025-01-21 09:53:59 -0500

    Update model_utils.py

--- src/model_utils.py
+++ src/model_utils.py
@@ -1,1 +1,9 @@
 
+import json
+
+class ModelUtils:
+    @staticmethod
+    def save_to_jsonl(data, output_path):
+        with open(output_path, "w") as f:
+            for item in data:
+                f.write(json.dumps(item) + "\n")
